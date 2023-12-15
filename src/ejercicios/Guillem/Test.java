8caf022aaa35085257afb7fc1f843c352e767e70
willy <willy@10.1.86.57>
2023-12-15 14:26:32 +0100

    15/12/23 *Ns como se hace*

--- src/ejercicios/Guillem/Test.java
+++ src/ejercicios/Guillem/Test.java
@@ -1,10 +1,26 @@
 package ejercicios.Guillem;
 
+import java.util.*;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-
+		Scanner sc = new Scanner(System.in);
+		
+		int bloq;
+		
+		do {
+			bloq = sc.nextInt();
+			int pisos = 1;
+			
+			for (int i = 1; i < bloq; i = i + 2) {
+				if (bloq == i * i) {
+					pisos++;
+				}
+			}
+			
+			System.out.println(pisos);
+		} while (bloq != 0);
 	}
 
 }
