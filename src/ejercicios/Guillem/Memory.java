562f711655d26ff7d0dde400c509336c36547808
willy <willy@CFGS-pc>
2023-12-19 12:25:35 +0100

    19/12/23

--- src/ejercicios/Guillem/Memory.java
+++ src/ejercicios/Guillem/Memory.java
@@ -1,10 +1,12 @@
 package ejercicios.Guillem;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Memory {
 
 	public static Scanner sc = new Scanner(System.in);
+	public static Random rd = new Random();
 	public static final int TAULER = 4;
 
 	public static void main(String[] args) {
@@ -28,9 +30,11 @@ public class Memory {
 				Posar(secret);
 				break;
 			case 4:
-				Remenar();
+				Remenar(secret);
+				Mostrar(secret);
+				break;
 			}
-			
+
 		} while (opcio != 0);
 
 	}
@@ -41,13 +45,14 @@ public class Memory {
 		System.out.println("1. Inicialitzar Tauler");
 		System.out.println("2. Mostrar Tauler");
 		System.out.println("3. Posar Peces");
-		System.out.println("3. Remenar Peces");
+		System.out.println("4. Remenar Peces");
+		
 		do {
 			System.out.println("Escull una opcio: ");
 			opcio = sc.nextInt();
-			if (opcio > 3 || opcio < 0)
+			if (opcio > 4 || opcio < 0)
 				System.out.println("Opcio invalida");
-		} while (opcio > 3 || opcio < 0);
+		} while (opcio > 4 || opcio < 0);
 		return opcio;
 	}
 
@@ -87,10 +92,36 @@ public class Memory {
 		}
 
 	}
-	
-	private static void Remenar() {
-		
-	}
-	
 
+	private static void Remenar(char[][] secret) {
+
+		for (int i = 0; i < 100; i++) {
+
+			int filaV1;
+			int colV1;
+			int filaV2;
+			int colV2;
+			boolean repetir = false;
+
+			do {
+
+				filaV1 = rd.nextInt(TAULER);
+				colV1 = rd.nextInt(TAULER);
+				filaV2 = rd.nextInt(TAULER);
+				colV2 = rd.nextInt(TAULER);
+
+				if (filaV1 == filaV2 && colV1 == colV2) {
+					repetir = true;
+				} else {
+					repetir = false;
+				}
+
+			} while (repetir);
+
+			char V1 = secret[filaV1][colV1];
+			char V2 = secret[filaV2][colV2];
+			secret[filaV2][colV2] = V1;
+			secret[filaV1][colV1] = V2;
+		}
+	}
 }
