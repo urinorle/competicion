274b029bb2a6028a7e054dc78f9d9ff882dce9ea
urinorle <willytri2005@gmail.com>
2024-01-18 10:32:23 +0100

    primeroo

--- src/buscaminas_guillem/Menubu.java
+++ src/buscaminas_guillem/Menubu.java
@@ -1,10 +1,76 @@
 package buscaminas_guillem;
 
+import java.util.Scanner;
+
+import tresenratlla.Jugadors;
+
 public class Menubu {
+	static Scanner sc = new Scanner(System.in);
+	static final int mesura = (Integer) null;
 
 	public static void main(String[] args) {
-		
+
+		Players j1 = null;
+		Players j2 = null;
+
+		int a;
+
+		do {
+			menu();
+			a = sc.nextInt();
+			switch (a) {
+			case 1:
+
+				break;
+			case 2:
+				menu2();
+				int b = sc.nextInt();
+				do {
+					switch (b) {
+					case 1:
+						Players.definirjugador();
+						break;
+					case 2:
+						Players.definirjugador();
+						break;
+					case 3:
+						Players.definirjugador();
+						break;
+					case 4:
+						Players.definirjugador();
+						break;
+					case 0:
+						break;
+					}
+				} while (b != 0);
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 0:
+				break;
+			}
+
+		} while (a != 0);
 
 	}
 
+	private static void menu() {
+		System.out.println("1.- Mostrar Ajuda");
+		System.out.println("2.- Opcions");
+		System.out.println("3.- Jugar Partida");
+		System.out.println("4.- Veure Ranking");
+		System.out.println("0.- Sortir");
+
+	}
+
+	private static void menu2() {
+		System.out.println("1.- Nivel principiant: 8 x 8 caselles i 10 mines");
+		System.out.println("2.- Nivel intermig: 16 x 16 caselles i 40 mines");
+		System.out.println("3.- Nivel expert: 16 x  30 caselles i 99 mines");
+		System.out.println("4.- Nivel personalitzat");
+		System.out.println("0.- Sortir");
+
+	}
 }
---
+++ src/buscaminas_guillem/Players.java
@@ -0,0 +1,29 @@
+package buscaminas_guillem;
+
+import java.util.Scanner;
+
+
+public class Players {
+	String nom;
+	int partidesWin = 0;
+	int partidesLose = 0;
+	
+	static void veureJugador(Players j1, Players j2) {
+		if (j1 != null) {
+			System.out.println("El jugador " + j1.nom + " ha guanyat " + j1.partidesWin + " ha perdut: " + j1.partidesLose);
+		}
+		if (j1 == null) {
+			System.out.println("Escull primer el jugador!!");
+		}
+	}
+	
+	static Players definirjugador() {
+		Players j = new Players();
+		System.out.println("Digues el nom: ");
+		Scanner sc = new Scanner(System.in);
+		j.nom = sc.nextLine();
+		j.partidesWin = 0;
+		j.partidesLose = 0;
+		return j;
+	}
+}
